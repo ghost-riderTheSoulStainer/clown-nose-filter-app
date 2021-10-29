25a7da498e405364ae05550e0770c840530f698e
ghost-riderTheSoulStainer <74955611+ghost-riderTheSoulStainer@users.noreply.github.com>
2021-10-29 20:43:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,4 +4,22 @@ function take_snapshot(){
 function setup(){
     canvas=createCanvas(300,300);
     canvas.center();
+    picture=createCapture(VIDEO);
+    picture.hide();
+    picture.size(300,300);
+    posenet=ml5.poseNet(picture,modelloaded);
+    posenet.on('pose',gotposes);
+}
+function draw(){
+    image(picture,0,0,300,300);
+}
+function modelloaded(){
+    console.log("posenet is initialized");
+}
+function gotposes(results){
+    if(results.length>0){
+        console.log(results);
+        console.log("Nose x="+results[0].pose.nose.x);
+        console.log("Nose y="+results[0].pose.nose.y);
+    }
 }
